68616cc4fda16eaf539a9f01d6c72c454ab76c21
joseespiritu <joseespiritu@aragon.unam.mx>
2020-04-18 23:06:02 -0500

    tipos de datos basicos

---
+++ tipos de datos basicos.cpp
@@ -0,0 +1,19 @@
+//Tipos de datos basicos de C++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int entero = 15;
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	char letra = 'a';
+	
+	cout<<entero<<endl;
+	cout<<flotante<<endl;
+	cout<<mayor<<endl;
+	cout<<letra<<endl;
+	
+	//se recomienda el uso del return 0
+	return 0;
+}
