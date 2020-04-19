3ae747324f741de19a2467bd50c6acd74b65be5d
joseespiritu <joseespiritu@aragon.unam.mx>
2020-04-18 22:58:57 -0500

    Hola mundo en C++ y prueba en visual studio code

---
+++ hola mundo.cpp
@@ -0,0 +1,14 @@
+//Primer programa en C++
+
+#include <iostream>
+
+//Std para uso de la consola
+using namespace std;
+
+int main(){
+	
+	//Imprimir en consola con salto de linea
+	cout<<"Hola mundo :D"<<endl;
+	
+	return 0;
+}
