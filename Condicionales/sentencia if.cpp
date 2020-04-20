75401487a7007f1828477a7380451937cdf1297a
joseespiritu <joseespiritu@aragon.unam.mx>
2020-04-19 21:28:19 -0500

    sentencia if

---
+++ Condicionales/sentencia if.cpp
@@ -0,0 +1,19 @@
+// Sentencia if
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int numero, dato = 5;
+	
+	cout<<"Digite un numero: "; cin>>numero;
+	
+	if (numero == dato){
+		cout<<"El numero es 5";
+	} else {
+		cout<<"El numero es diferente de 5";
+	}
+	
+	return 0;
+}
