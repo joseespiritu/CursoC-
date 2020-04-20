34ba7eb12883a6f6f75dc6bff6375973caa95839
joseespiritu <joseespiritu@aragon.unam.mx>
2020-04-19 21:21:34 -0500

    funciones

---
+++ funciones.cpp
@@ -0,0 +1,19 @@
+// Ejecicio de funcion para unos valores dados de x e y
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	float x,y,resultado=0;
+	
+	cout<<"Digite el valor de x: "; cin>>x;
+	cout<<"Digite el valor de y: "; cin>>y;
+	
+	resultado = (sqrt(x))/(pow(y,2)-1);
+	
+	cout<<"El resultado es: "<<resultado;
+	
+	return 0;
+}
